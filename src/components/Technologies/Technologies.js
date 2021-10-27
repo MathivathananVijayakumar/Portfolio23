759517dff4c133476a6f384e5cf46f59c336ef48
Mathivathanan <43021541+MathivathananVijayakumar@users.noreply.github.com>
2021-10-27 16:07:54 +0530

    Add files via upload

---
+++ src/components/Technologies/Technologies.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { DiFirebase, DiReact } from 'react-icons/di';
+import { Section, SectionDivider, SectionText, SectionTitle } from '../../styles/GlobalComponents';
+import { List, ListContainer, ListItem, ListParagraph, ListTitle } from './TechnologiesStyles';
+
+const Technologies = () =>  (
+  <Section id="tech">
+    <SectionDivider divider />
+    <SectionTitle>Technologies</SectionTitle>
+    <SectionText>
+      I'm a new tech bee and interested to learn web development.
+      From Back-end To Design.
+    </SectionText>
+    <List>
+      <ListItem>
+        <picture>
+          <DiReact size="3rem" />
+        </picture>
+        <ListContainer>
+          <ListTitle>Front-End</ListTitle>
+          <ListParagraph>
+            Experience with <br />
+            React.js
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <picture>
+          <DiFirebase size="3rem" />
+        </picture>
+        <ListContainer>
+          <ListTitle>Back-End</ListTitle>
+          <ListParagraph>
+            Experience with <br />
+            Node and Databases
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+     
+    </List>
+    <SectionDivider colorAlt />
+  </Section>
+);
+
+export default Technologies;
---
+++ src/constants/constants.js
@@ -0,0 +1,35 @@
+export const projects = [
+  {
+    title: 'MERN Crud App',
+    description: "Using React, Node.js, Express & MongoDB  this project gives an idea about how to develop Mern Project",
+      tags: ['Mongo', 'Express', 'React', 'Node'],
+    visit: 'https://github.com/MathivathananVijayakumar/Crud-using-Mern',
+    id: 0,
+  },
+  {
+    title: 'E-Commerce',
+    description:" Using React we have built the basic E-commerce like App and it will give the depth of understanding project.",
+    tags: ['React'],
+    visit: 'https://github.com/MathivathananVijayakumar/Smallshop-new',
+    id: 1,
+  },
+  {
+    title: 'Contact Manager App',
+    description: "This is basic how to do crud operations with react it will give clear idea about react and its usage",
+      tags: ['React', 'Axios'],
+    visit: 'https://github.com/MathivathananVijayakumar/contact-manager',
+    id: 2,
+  },
+  {
+    title: 'Chatbot',
+    description: "This is like a ChatApp which is used to communicate with friends which is done using React and ChatApi.",
+   tags: ['React', 'ChatEngineApi'],
+   visit: 'https://github.com/MathivathananVijayakumar/chat',
+    id: 3,
+  },
+];
+
+export const TimeLineData = [
+  { year: 2021 , text: 'Started my journey and Learning Many things' }
+
+];
